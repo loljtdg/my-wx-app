cb445ee54c469bf80affc2dc254ee5e8fc810951
zhangyifei <zhangyifei.sky@bytedance.com>
2021-10-27 21:15:28 +0800

    feat: add update self

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,36 @@ import { Component } from "react";
 import Taro from "@tarojs/taro";
 import "./app.less";
 
+const doUpdate = () => {
+  const updateManager = Taro.getUpdateManager();
+
+  updateManager.onCheckForUpdate(function(res) {
+    // 请求完新版本信息的回调
+    console.log(res.hasUpdate);
+  });
+
+  updateManager.onUpdateReady(function() {
+    Taro.showModal({
+      title: "更新提示",
+      content: "新版本已经准备好，是否重启应用？",
+      success(res) {
+        if (res.confirm) {
+          // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+          updateManager.applyUpdate();
+        }
+      }
+    });
+  });
+
+  updateManager.onUpdateFailed(function() {
+    // 新版本下载失败
+  });
+};
+
 class App extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    doUpdate();
+  }
 
   componentDidShow() {}
 
@@ -12,7 +40,7 @@ class App extends Component {
   componentDidCatchError() {}
 
   onLaunch() {
-    Taro.cloud.init()
+    Taro.cloud.init();
   }
 
   // this.props.children 是将要会渲染的页面
